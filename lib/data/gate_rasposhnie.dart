d7cef62df1e41a90a20fbd9da1fbe219123d6e1c
ElcimatAmir <elcimatamir@gmail.com>
2023-02-20 11:00:04 +0300

    first add

---
+++ lib/data/gate_rasposhnie.dart
@@ -0,0 +1,15 @@
+// class for the расспошные ворота
+class GateRasposhnie {
+  //data that constant ffor the price
+  // Бурение Лунок
+  int _bu_lunok = 250;
+
+  // установка столбов
+  int _installing_pillers = 500;
+
+  //изготовления коркаса
+  int _createing_karkas = 15000;
+
+  // подключения автоматики
+  int _add_avtomatic = 6000;
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'screens/calculater_screen.dart';
+import 'screens/history_screen.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    int screeenNumber = 0;
+    List<Widget> screens = [CalculaterSreen(), const HistoryScreen()];
+    return SafeArea(
+      child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("calculater");
+                    setState(() {
+                      screeenNumber = 0;
+                    });
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.calculate,
+                          size: 30,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Text('Рассчитать стоимость'),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print("history");
+                    setState(() {
+                      screeenNumber = 1;
+                    });
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.history,
+                          size: 30,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Text('История Рассчетов'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )),
+        Expanded(
+          flex: 2,
+          child: screens[screeenNumber],
+        )
+      ]),
+    );
+  }
+}
